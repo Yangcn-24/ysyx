e8e4919059c3202fdc81b96db7ef48b1bcefc082
yangcn <cnyang24@mails.jlu.edu.cn>
2025-10-04 00:26:32 +0800

    write runtests.sh

--- c/ex29/tests/runtests.sh
+++ c/ex29/tests/runtests.sh
@@ -1,13 +1,24 @@
 echo "Running unit tests:"
 library="./build/libex29.so"
-function=("print_a_message" "uppercase" "lowercase ")
+test_cases=(
+    "print_a_message" "hello there"
+    "uppercase" "hello there"
+    "lowercase" "HELLO tHeRe"
+    "fail_on_purpose" "i fail"
+    "fail_on_purpose" ""
+    "adfasfasdf" "asdfadff"
+    "adfasfasdf" "asdfadfas"
+)
 for i in bin/*_tests
 do
-	for function in "${function[@]}"
-	do
-	    if test -f $i
-	    then
-		if $VALGRIND ./$i $library $function "hello there" 2>> tests/tests.log
+    if test -f $i
+    then
+    	for ((j=0; j < ${#test_cases[@]}; j+=2))
+    	do
+    		command=${test_cases[j]}
+    		argument=${test_cases[j+1]}
+    		echo ./$i $library $command $argument
+		if $VALGRIND ./$i $library $command "$argument" 2>> tests/tests.log
 		then
 		    echo $i PASS
 		else
@@ -16,8 +27,8 @@ do
 		    tail tests/tests.log
 		    exit 1
 		fi
-	    fi
 	done
+    fi
 done
 
 echo ""
